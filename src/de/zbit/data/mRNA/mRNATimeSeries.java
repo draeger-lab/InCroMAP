22cc4483cdc33b2b9761e8f9eb641eb17c9349c1
Felix Bartusch <felix.bartusch@uni-tuebingen.de>
2014-04-14 08:17:53 +0000

    Added comments

--- src/de/zbit/data/mRNA/mRNATimeSeries.java
+++ src/de/zbit/data/mRNA/mRNATimeSeries.java
@@ -1,14 +1,35 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of Integrator, a program integratively analyze
+ * heterogeneous microarray datasets. This includes enrichment-analysis,
+ * pathway-based visualization as well as creating special tabular
+ * views and many other features. Please visit the project homepage at
+ * <http://www.cogsys.cs.uni-tuebingen.de/software/InCroMAP> to
+ * obtain the latest version of Integrator.
+ *
+ * Copyright (C) 2011 by the University of Tuebingen, Germany.
+ *
+ * Integrator is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
 package de.zbit.data.mRNA;
 
 import de.zbit.data.Signal;
 import de.zbit.data.id.GeneID;
 
-
 /**
  * A class to hold {@link mRNA} probes with {@link Signal}s and {@link GeneID}s.
- * There are no new methods/fields in this class. With this class the toolbar can visualize specific Buttons for
+ * There are currently no new methods/fields in this class. With this class the toolbar can visualize specific Buttons for
  * time series data.
  * @author Felix Bartusch
+ * @version $Rev$
  */
 public class mRNATimeSeries extends mRNA {
 
