8260dff229f1c166f4317b28149b4a97ae50b141
Felix Bartusch <felix.bartusch@uni-tuebingen.de>
2014-06-02 07:49:30 +0000

    - changed from interface to abstract class - Needed some information of the models, so I added some more fields with getter and setter - added also another constructor for the new fields

--- src/de/zbit/math/TimeSeriesModel.java
+++ src/de/zbit/math/TimeSeriesModel.java
@@ -33,15 +33,104 @@ import de.zbit.util.objectwrapper.ValueTriplet;
  * @author Felix Bartusch
  * @version $Rev$
  */
-public interface TimeSeriesModel {
+public abstract class TimeSeriesModel {
+	
+	/**
+	 * Name of the modeled gene.
+	 */
+	String name;
+	/**
+	 * geneID of the modeled gene.
+	 */
+	int geneID;
+	/**
+	 * Type of the modeled values. (p-value or fold change)
+	 */
+	SignalType signalType;
+	/**
+	 * The x-values for the given points.
+	 */
+	double[] x;
+	/**
+	 * The y-values for the given points.
+	 */
+	double[] y;
+	
+	
+	public TimeSeriesModel() {
+		this.name = null;
+		this.geneID = -1;
+	}
+	
+	/**
+	 * @param name of the modeled gene.
+	 * @param geneID of the modeled gene.
+	 */
+	public TimeSeriesModel(String name, int geneID, SignalType type) {
+		this.name = name;
+		this.geneID = geneID;
+		this.signalType = type;
+	}
 	
 	/**
 	 * Generate the model for one gene.
 	 */
-	public void generateModel(mRNATimeSeries dataPoints, List<ValueTriplet<Double, String, SignalType>> timePoints);
+	public abstract void generateModel(mRNATimeSeries dataPoints, List<ValueTriplet<Double, String, SignalType>> timePoints);
 	
 	/**
 	 * Compute the interpolated value for certain coefficients at a certain time point.
 	 */
-	public double computeValueAtTimePoint(double timePoint);	
+	public abstract double computeValueAtTimePoint(double timePoint);	
+	
+	/**
+	 * @return the first timePoint that can be modelled by this model.
+	 */
+	public double getFirstTimePoint() {
+		return x[0];
+	}
+	
+	/**
+	 * @return the last timePoint that can be modelled by this model.
+	 */
+	public double getLastTimePoint() {
+		return x[x.length-1];
+	}
+
+	/**
+	 * @return name of the modeled gene.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return geneID of the modeled gene.
+	 */
+	public int getGeneID() {
+		return geneID;
+	}
+	
+	/**
+	 * @return signal type of the modeled values. (SignalType.FoldChange or SignalType.pValue)
+	 */
+	public SignalType getSignalType() {
+		return signalType;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setGeneID(int geneID) {
+		this.geneID = geneID;
+	}
+
+	public void setSignalType(SignalType signalType) {
+		this.signalType = signalType;
+	}
+	
+	public double[] getY() {
+		return y;
+	}
 }
+
